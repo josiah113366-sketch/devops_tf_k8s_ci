ce7d71b2d796ccb1a9b5f0bbc6da7237d780bebe
josiah113366-sketch <josiah113366@gmail.com>
2026-08-06 10:24:50 +0900

    Feat: OIDC 관련 기본 정보 조회 및 arn 주소 구성

--- infra/github-actions-ci.tf
+++ infra/github-actions-ci.tf
@@ -1,10 +1,22 @@
-data "aws_caller_identity" "current" {}
+# ─────────────────────────────────────────────
+# Github Action이 장기키(pem, access key)없이 임시 자격 증명(OIDC)을 이용 인증 역활,권한등 처리
+# 발급받아서 WEB/WAS 이미지를 ECR에 PUSH 목적 -> 보안 관리 이슈 x (깃상에 변수등이라도 존재 x)
+# 
+# 누가 발급한 인증 토큰을 신뢰할것인가?(발급 기관), Github에서 어떤 실행를 할때 인증 허가할것인가?
+# 이 2개의 대한 내용을 role에 반영하여 arn 구성
+# ─────────────────────────────────────────────
 
+# 현재 어떤 사용자(IAM)/역활등이 실행중이지 => data => 조회후 채움
+data "aws_caller_identity" "current" {}
+# AWS 영역 구분값 조회
 data "aws_partition" "current" {}
 
 locals {
+  # 위의 값을 조회하여 oidc를 발급하는 공급자의 arn 생성
   github_actions_oidc_provider_arn = "arn:${data.aws_partition.current.partition}:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/token.actions.githubusercontent.com"
 
+  # IAM Role에서 사용가능한 Github 저장소, 브런치 지정 (소유자, 저장소, 브런치)
+  # 2026년 7월 이후 변경된 주소 체계때문에 이전과 같이 배치 -> 보안강화됨
   #github_actions_ci_subject = "repo:${var.github_owner}/${var.github_ci_repository}:ref:refs/heads/${var.github_ci_branch}"
   github_actions_ci_subject = "repo:${var.github_owner}@${var.github_owner_id}/${var.github_ci_repository}@${var.github_ci_repository_id}:ref:refs/heads/${var.github_ci_branch}"
 }
